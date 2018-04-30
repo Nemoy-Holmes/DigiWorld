ad19b39650f7e0b088068aff86429aee8dd0386a
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-05-01 00:01:55 +0200

    5.9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery.raty.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require jquery.raty.js
 $(document).on('turbolinks:load', function(){
 	setTimeout(function(){
 		$(".alert").fadeOut("slow",function(){
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, presence: true
+
  scope :rating_desc, -> { order(rating: :desc) } 
 end
