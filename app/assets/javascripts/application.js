38824c295fe9fd416a5772de66caae7aa5cb78de
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-05-02 12:40:15 +0200

    5.8 test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require jquery.raty.js
 //= require rails-ujs
 //= require turbolinks
+//= require jquery3
+//= require jquery.raty.js
 //= require_tree .
-
 $(document).on('turbolinks:load', function(){
     setTimeout(function(){
         $(".alert").fadeOut("slow",function(){
