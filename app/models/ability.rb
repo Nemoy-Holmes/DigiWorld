76970c237cf281fdfc8236abf6e079cbae246c1c
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-05-04 16:16:05 +0200

    5.10.admin-control

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. 
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-   if user ||= User.new #guest (not loggeg in)
-    can :manage, User, id: user.id
-    can :read, Product
-    cannot :manage, Order
-    if user.admin?
-    	can :manage, :all
-    end
+   user ||= User.new #guest (not loggeg in)
+   if user.admin?
+    can :manage, :all
+  else
+    can :read, :all
+    can :manage, :User, id: user.id
+    cannot :manage, :Product
     end
   end
 end
