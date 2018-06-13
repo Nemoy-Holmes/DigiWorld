016fae67d3dbe5999073f2d31f41a4d34ba3d4f7
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-06-13 07:09:22 +0200

    6.10

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
 			if @comment.save
 				format.html {redirect_to @product, notice: 'review was created successfully.' }
 				format.json { render :show, status: :created, location: @product }
+				format.js
 			else 
 				format.html { redirect_to @product, alert: 'review was not saved successfully.'}
 				format.json { render json: @comment.errors, status: :unprocessable_entity }
