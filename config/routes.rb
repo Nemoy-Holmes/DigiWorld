e3a957d4fd13bb93d52c9e64249cc84cdf35e46c
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-07-20 23:03:26 +0200

    rout6.14

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'payments/create'
-  
-
 
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
