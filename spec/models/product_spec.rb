c9bc2b6c9ce8fd75fde0156ee07d69f75c075e40
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-07-25 12:55:29 +0200

    6.15 failing test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,35 @@
 require 'rails_helper'
 
 describe Product do
- before do
-   @product = Product.create!(name: "Race Bike")
-   @user = User.create(email: "test1@test.com", password: "test1234")
-   @product.comments.create!(rating: 1, user: @user, body: "Awlful bike!")
-   @product.comments.create!(rating: 3, user: @user, body: "This is a great bike!")
-   @product.comments.create!(rating: 5, user: @user, body: "This product is fantastic")
- end
- 
- it "returns the average rating of the comments" do
-   expect(@product.average_rating).to eq 3
- end
- 
- it "shouldn't create a product becasue there is a missing name" do
-   expect(Product.new(description: "Great Bike")).to_not be_valid
- end
+
+  before do
+    @product = FactoryBot.create(:product)
+    @user = FactoryBot.create(:user)
+    @product.comments.create!(rating: 1, user: @user, body: "Awful !")
+    @product.comments.create!(rating: 3, user: @user, body: "Ok !")
+    @product.comments.create!(rating: 5, user: @user, body: "Great !")
+  end
+
+  context "when product has comments" do
+    it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq (3)
+    end
+
+    it "returns the highest rating of all comments" do
+      expect(@product.highest_rating_comment).to eq (5)
+    end
+
+  end
+
+  context "when product is created" do
+
+    it "is invalid without a name" do
+      expect(Product.new(description: "Nice bike")).not_to be_valid
+    end
+
+    it "is valid with a name" do
+      expect(Product.new(name: "race bike")).to be_valid
+    end
+  end
+
 end
